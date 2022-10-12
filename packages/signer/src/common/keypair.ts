4cb384d8ff478dbe79af93de82e764d051ce4366
stfy <rustfy@gmail.com>
2022-10-12 11:49:47 +0300

    chore(signer): added ability to create keypair fom private key

--- packages/signer/src/common/keypair.ts
+++ packages/signer/src/common/keypair.ts
@@ -7,22 +7,25 @@ import {
     strToBytes,
     toBase58
 } from '@wavesenterprise/crypto-utils';
-import {sha256} from "../utils/sha256";
+import {fromBase58, generateKeyPair} from "@wavesenterprise/crypto-utils";
 
 
 export class Keypair {
     constructor(
         private keyPairBytes: KeyPairBytes,
-        private _phrase: string,
+        private _phrase?: string,
         private networkByte: number = 'V'.charCodeAt(0)) {
     }
-
 
     setNetworkByte(byte: number) {
         this.networkByte = byte;
     }
 
     phrase(): string {
+        if (!this._phrase) {
+            throw new Error('Keypair created from private key')
+        }
+
         return this._phrase
     }
 
@@ -34,18 +37,24 @@ export class Keypair {
         return toBase58(this.keyPairBytes.privateKey)
     }
 
-    async address(): Promise<string> {
-        return toBase58(createAddress(this.keyPairBytes.publicKey, this.networkByte))
+    async address(networkByte?: number): Promise<string> {
+        return toBase58(createAddress(this.keyPairBytes.publicKey, networkByte || this.networkByte))
     }
 
     static async generate(words = 15) {
-        const seed = await createSeedPhrase(sha256, words)
+        const seed = await createSeedPhrase(words)
 
         return this.fromExistingSeedPhrase(seed.join(' '))
     }
 
+    static async fromString(privateKey: string) {
+        const keyPair = generateKeyPair(fromBase58(privateKey))
+
+        return new Keypair(keyPair)
+    }
+
     static async fromExistingSeedPhrase(phrase: string) {
-        const keys = await createKeys(sha256, phrase);
+        const keys = await createKeys(phrase);
 
         return new Keypair(keys, phrase)
     }
--- packages/signer/src/utils/sha256.ts
+++
@@ -1,5 +0,0 @@
-import { createHash } from 'crypto'
-
-export const sha256 = (data: Uint8Array) => {
-    return createHash('sha256').update(data).digest()
-}
